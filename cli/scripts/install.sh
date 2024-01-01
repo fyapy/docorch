f85a68215b773a2c6d7ffd53884c9bdfb3aaf5f0
Amir Abdullin <amirop35@gmail.com>
2024-01-01 07:20:07 +0300

    WIP install script

--- cli/scripts/install.sh
+++ cli/scripts/install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+# Run script:
+# curl -sSL https://raw.githubusercontent.com/fyapy/docorch/master/cli/scripts/install.sh | sh
 
-echo "Docorch"
+curl -sSL https://github.com/fyapy/docorch/raw/master/cli/docli.zip -o /var/www/docli.zip
+
+apt install unzip -y
+
+unzip /var/www/docli.zip
+chmod +x /var/www/docli
+
+ls /var/www
