ac252f88642899c8c208f739165990a8aba71cf7
Amir Abdullin <amirop35@gmail.com>
2024-02-01 06:01:49 +0300

    cli update install script

--- cli/scripts/install.sh
+++ cli/scripts/install.sh
@@ -2,9 +2,7 @@
 mkdir -p /etc/docorch
 curl -sSL https://github.com/fyapy/docorch/raw/master/cli/doctl -o /usr/bin/doctl
 
-chmod +x /etc/docorch/doctl
-
-mv -f /etc/docorch/doctl /usr/bin/doctl
+chmod +x /usr/bin/doctl
 
 curl -sSL https://get.docker.com | sh
 usermod -aG docker $(whoami)
